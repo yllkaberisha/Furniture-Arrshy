0b316275e69bcecaa6f318d21bf43582734f52dd
220765100073 <shqiponje.dani@student.uni-pr.edu>
2023-12-28 13:11:26 +0100

    orderjs

--- js/order.js
+++ js/order.js
@@ -1,35 +1,82 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.forms.order;
+const form = document.getElementById('form');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const productType = document.getElementById('tableType');
+const quantity = document.getElementById('guestNumber');
+const color = document.getElementById('Color');
+const note = document.getElementById('note');
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent the form from submitting
 
-        // Perform validation
-        const firstName = form.elements["First Name"].value.trim();
-        const lastName = form.elements["Last Name"].value.trim();
-        const email = form.elements["Email"].value.trim();
+//show error function
+function showError(input, message){
+    const formgroup= input.parentElement;
+    formgroup.className ='form__group error' ;
+    const small = formgroup.querySelector('small');
+    small.innerText= message;
 
-        // Simple validation for first name and last name (non-empty)
-        if (firstName === "") {
-            alert("Please enter your first name.");
-            return;
-        }
+}
+//show sucess function
+function showSuccess(input){
+    formgroup= input.parentElement;
+    formgroup.className ='form__group success' ;
+}
+//email function
+function checkEmail(input){
+    var re = /\S+@\S+\.\S+/;
+ if(re.test(input.value.trim())){
+    showSuccess(input);
+ }else{
+    showError(input, 'Email is not valid');
+ }
+}
+// check required
 
-        if (lastName === "") {
-            alert("Please enter your last name.");
-            return;
-        }
-
-        // Email validation using a regular expression
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(email)) {
-            alert("Please enter a valid email address.");
-            return;
-        }
-
-        // If all validation passes, you can submit the form or perform other actions
-        alert("Form submitted successfully!");
-        // Uncomment the following line to submit the form
-        // form.submit();
+function checkRequired(inputArray){
+    inputArray.forEach(function(input){
+     if(input.value===''){
+        showError(input, `${getFieldName(input)} is Reequired `)
+     }
     });
+
+}
+//check length
+function checkLength(input, min,max){
+    if(input.value.length<min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    
+    }else if(input.value.length>max){
+    showError(input, `${getFieldName(input)} must be at less than ${max} characters`)
+}
+else{
+    showSuccess(input);
+}
+}
+// get field name 
+function  getFieldName(input){
+    return input.id.charAt(0).toUpperCase()+input.id.slice(1);
+}
+
+// Even listeners
+form.addEventListener( 'submit', function(e){
+    e.preventDefault();
+    checkRequired([firstName,lastName,email,productType,color,quantity,note]);
+    checkLength(note, 10, 20);
+    checkEmail(email);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
