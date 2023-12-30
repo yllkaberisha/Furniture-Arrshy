b9ec26cfe6cf4f104f7460c8e897142dcd393c40
yllkeberisha <116629138+yllkeberisha@users.noreply.github.com>
2023-12-30 13:55:23 +0100

    submit form and send it to email

--- js/main.js
+++ js/main.js
@@ -35,3 +35,27 @@ window.addEventListener('resize', () => {
     navClose();
   }
 });
+
+
+
+var form = document.getElementById("contact");
+async function handleSubmit(event) {
+event.preventDefault();
+var status = document.getElementById("my-form-status");
+var data = new FormData(event.target);
+fetch(event.target.action, {
+  method: form.method,
+  body: data,
+  headers: {
+    'Accept': 'application/json'
+}
+}).then(response => {
+  if (response.ok) {
+    status.innerHTML = "Thanks for your submission!";
+    form.reset()
+  } 
+}).catch(error => {
+  status.innerHTML = "Oops! There was a problem submitting your form"
+});
+}
+form.addEventListener("submit", handleSubmit)
